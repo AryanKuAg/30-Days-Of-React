ed6c7816e7725bf4c398c4fe53aec7ca5c1f6e0d
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-03-02 14:05:57 +0530

    Array completed

--- 01_Day_JavaScript_Refresher/array.js
+++ 01_Day_JavaScript_Refresher/array.js
@@ -1,4 +1,38 @@
 // const myArray = new Array(10);
-const myArray = new Array(10).fill(1);
+// const myArray = new Array(10).fill(1);
 
-console.log(myArray);
+// console.log(myArray);
+
+// const fixedArray = new Array();
+// fixedArray = "sfsf"; not going to work because of const
+// fixedArray.push("the ting");
+// fixedArray.push(343553);
+// console.log(fixedArray);
+
+// const array = [35, 35, 353, 432, 5, 324];
+// console.log(array.length);
+
+// const name = "aryan agrawal";
+// const array = name.split(" ");
+// array[0] = "am";
+// console.log(array);
+
+// There are number of ways to manipulate an array
+
+// const array = new Array(8).fill(50);
+// const array2 = new Array(3).fill(100);
+// const concate = array.concat(array2);
+// console.log(array.splice(2, 5, "aryan", "agarawal", "sdf", "sdf"));
+// console.log(array);
+
+// const array = [5].fill(35);
+// console.log(array);
+
+// const array = [34, 35, 342, 5, 24, 34];
+// array.unshift(343);
+// console.log(array);
+// console.log(array.reverse());
+
+// const array = [3, 53, 6, 35];
+// array.sort((a, b) => a - b);
+// console.log(array);
